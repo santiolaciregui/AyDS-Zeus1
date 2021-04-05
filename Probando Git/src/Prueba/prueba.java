66c36a8e4ef5e61ccac7fd208420c7cb3ca14041
Ezequiel Zucoli <42452277+zucolieze@users.noreply.github.com>
2021-04-05 10:23:33 -0300

    Update prueba.java

--- Probando Git/src/Prueba/prueba.java
+++ Probando Git/src/Prueba/prueba.java
@@ -3,5 +3,7 @@ package Prueba;
 public class prueba {
 	public static void main(String args[]) {
 		System.out.println("Probando git con Santiago, "+" Ezequiel, "+" Matias ");
+		
+		System.out.println("test conflico 1");
 	}
 }
